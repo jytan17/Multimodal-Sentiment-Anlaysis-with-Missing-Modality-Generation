a7bc6ae4062c1c4b5870058c75daf8632c72c1b4
jytan17 <87595774+jytan17@users.noreply.github.com>
2021-09-15 11:48:11 -0700

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -37,16 +37,16 @@ get_test_data(exp_config)
 get_sim_src_tgt(exp_config)
 
 # generative model config.
-model_config = {
+gen_model_config = {
     "batch_size": random.choice([32,64]),
     "lr": random.choice([0.0001, 0.0005, 0.001]),
-    "hidden_size": random.choice([64, 128, 256]),
-    "dropout": random.choice([0.2, 0.5, 0.8]),
+    "hidden_size": random.choice([64, 128, 256, 512]),
+    "dropout": random.choice([0.2, 0.5, 0.7]),
     "epochs": 100,
-    "enforce_rate": random.choice([0.2, 0.5, 0.7])
+    "enforce_rate": random.choice([0, 0.2, 0.5, 0.7])
     }
 # prediction model config.
-gen_model_config = {
+model_config = {
     "batch_size": random.choice([32, 64]),
     "lr": random.choice([0.0001 ,0.0005, 0.001]),
     "hidden_size": random.choice([64, 128, 256, 512]),
